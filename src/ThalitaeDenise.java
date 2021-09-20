9a591897705a1ae111db4e5c9dd706fd04abd356
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-20 17:16:09 -0300

    do while no menu

--- src/ThalitaeDenise.java
+++ src/ThalitaeDenise.java
@@ -14,105 +14,175 @@ _ Deve ser criado um novo repositório no github para a dupla e inserido o usuá
 import java.util.Scanner;
 
 public class ThalitaeDenise {
-    public static void main(String args []){
+    public static void main(String args []) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("=== BEM VINDE ===\n1 - Para números pares\t|\t2 - Para números ímpares\t|\t3 - Para números primos\t|\t" +
+
+        boolean continua = true;
+        char opcContinua;
+
+        do {
+
+        System.out.print("\n=== BEM VINDE ===\n1 - Para números pares\t|\t2 - Para números ímpares\t|\t3 - Para números primos\t|\t" +
                 "4 - Para calcular juros compostos\t|\t5 - Sair\nDigite a opção desejada: ");
 
         char menu = sc.next().charAt(0);
-        while (menu != '1' && menu != '2' && menu != '3' && menu != '4' && menu != '5'){
+        while (menu != '1' && menu != '2' && menu != '3' && menu != '4' && menu != '5') {
             System.out.print("Opção inválida. Digite novamente: ");
             menu = sc.next().charAt(0);
         }
 
         int entrada = 0;
 
-        switch (menu){
-            case '1':
-                System.out.print("=== NÚMEROS PARES ===\nInforme um valor: ");
-                entrada = sc.nextInt();
-                while (entrada < 0){
-                    System.out.print("Valor inválido. Digite novamente: ");
+            switch (menu) {
+                case '1':
+                    System.out.print("=== NÚMEROS PARES ===\nInforme um valor: ");
                     entrada = sc.nextInt();
-                }
-                for (int i = 0; i <= entrada; i++){
-                    if (i % 2 == 0){
-                        System.out.printf("%d \t|\t", i);
+                    while (entrada < 0) {
+                        System.out.print("Valor inválido. Digite novamente: ");
+                        entrada = sc.nextInt();
                     }
-                }
-                break;
-            case '2':
-                System.out.print("=== NÚMEROS IMPARES ===\nInforme um valor: ");
-                entrada = sc.nextInt();
-                while (entrada < 0){
-                    System.out.print("Valor inválido. Digite novamente: ");
-                    entrada = sc.nextInt();
-                }
-                for (int i = 0; i <= entrada; i++){
-                    if (i % 2 == 1){
-                        System.out.printf("%d \t|\t", i);
-                    }
-                }
-                break;
-            case '3':
-                System.out.print("=== NÚMEROS PRIMOS ===\nInforme um valor: ");
-                entrada = sc.nextInt();
-                while (entrada < 0){
-                    System.out.print("Opção inválida. Digite novamente. ");
-                    entrada = sc.nextInt();
-                }
-                for(int i = 2; i < entrada; i++){
-                    boolean primo = true;
-                    for( int p = 2; p < i; p++ ){
-                        if(i % p == 0){
-                            primo = false;
+                    for (int i = 0; i <= entrada; i++) {
+                        if (i % 2 == 0) {
+                            System.out.printf("%d \t|\t", i);
                         }
                     }
-                    if(primo){
-                        System.out.print( i + " é primo" );
+
+                    System.out.print("\nDeseja continuar?\nDigite 1 para SIM ou 2 para NÃO: ");
+                    opcContinua = sc.next().charAt(0);
+
+                    while (opcContinua != '1' && opcContinua != '2') {
+                        System.out.print("Opção inválida. Digite novamente: ");
+                        opcContinua = sc.next().charAt(0);
                     }
-                }
-                break;
-            case '4':
-                double invest = 0.0;
-                int prazo = 0;
-                double taxa = 0.0;
-                double newInvest = invest;
-                double acresMes = 0.0;
 
-                System.out.println("Digite o valor inicial: R$ ");
-                invest = sc.nextDouble();
-                while (invest <= 0){
-                    System.out.print("V=R$ ");
+                    if (opcContinua == '1') {
+                        continua = true;
+                    } else {
+                        continua = false;
+                    }
+
+                    break;
+                case '2':
+                    System.out.print("\n=== NÚMEROS IMPARES ===\nInforme um valor: ");
+                    entrada = sc.nextInt();
+                    while (entrada < 0) {
+                        System.out.print("Valor inválido. Digite novamente: ");
+                        entrada = sc.nextInt();
+                    }
+                    for (int i = 0; i <= entrada; i++) {
+                        if (i % 2 == 1) {
+                            System.out.printf("%d \t|\t", i);
+                        }
+                    }
+
+                    System.out.print("\nDeseja continuar?\nDigite 1 para SIM ou 2 para NÃO: ");
+                    opcContinua = sc.next().charAt(0);
+
+                    while (opcContinua != '1' && opcContinua != '2') {
+                        System.out.print("Opção inválida. Digite novamente: ");
+                        opcContinua = sc.next().charAt(0);
+                    }
+
+                    if (opcContinua == '1') {
+                        continua = true;
+                    } else {
+                        continua = false;
+                    }
+
+                    break;
+                case '3':
+                    System.out.print("\n=== NÚMEROS PRIMOS ===\nInforme um valor: ");
+                    entrada = sc.nextInt();
+                    while (entrada < 0) {
+                        System.out.print("Opção inválida. Digite novamente. ");
+                        entrada = sc.nextInt();
+                    }
+                    System.out.println("NÚMEROS PRIMOS:");
+                    for (int i = 2; i < entrada; i++) {
+                        boolean primo = true;
+                        for (int p = 2; p < i; p++) {
+                            if (i % p == 0) {
+                                primo = false;
+                            }
+                        }
+                        if (primo) {
+                            System.out.print(i + "\t|\t");
+                        }
+                    }
+
+                    System.out.print("\nDeseja continuar?\nDigite 1 para SIM ou 2 para NÃO: ");
+                    opcContinua = sc.next().charAt(0);
+
+                    while (opcContinua != '1' && opcContinua != '2') {
+                        System.out.print("Opção inválida. Digite novamente: ");
+                        opcContinua = sc.next().charAt(0);
+                    }
+
+                    if (opcContinua == '1') {
+                        continua = true;
+                    } else {
+                        continua = false;
+                    }
+
+                    break;
+                case '4':
+                    double invest = 0.0;
+                    int prazo = 0;
+                    double taxa = 0.0;
+                    double newInvest = invest;
+                    double acresMes = 0.0;
+
+                    System.out.print("Digite o valor inicial: R$ ");
                     invest = sc.nextDouble();
-                }
+                    while (invest <= 0) {
+                        System.out.print("V=R$ ");
+                        invest = sc.nextDouble();
+                    }
 
-                System.out.println("Digite a taxa mensal (%): ");
-                taxa = sc.nextDouble();
-                while (taxa <= 0){
-                    System.out.print("Valor inválido. Digite novamente: ");
+                    System.out.print("Digite a taxa mensal (%): ");
                     taxa = sc.nextDouble();
-                }
+                    while (taxa <= 0) {
+                        System.out.print("Valor inválido. Digite novamente: ");
+                        taxa = sc.nextDouble();
+                    }
 
-                System.out.print("Digite o prazo de investimento em meses: ");
-                prazo = sc.nextInt();
-                while (prazo <= 0){
-                    System.out.print("Valor inválido. Digite novamente: ");
+                    System.out.print("Digite o prazo de investimento em meses: ");
                     prazo = sc.nextInt();
-                }
+                    while (prazo <= 0) {
+                        System.out.print("Valor inválido. Digite novamente: ");
+                        prazo = sc.nextInt();
+                    }
 
-                double taxaPercent = taxa / 100;
+                    double taxaPercent = taxa / 100;
 
-                for (int mes = 0; mes < prazo; mes++){
-                    acresMes = invest * taxaPercent;
-                    invest += acresMes;
-                    System.out.printf("Mes %d : R$ %.2f \n", mes + 1, invest);
-                }
-                break;
-            case '5':
-                System.out.print("=== FIM DO PROGRAMA ===");
-                break;
-        }
+                    for (int mes = 0; mes < prazo; mes++) {
+                        acresMes = invest * taxaPercent;
+                        invest += acresMes;
+                        System.out.printf("Mês %d : R$ %.2f \n", mes + 1, invest);
+                    }
+
+                    System.out.print("\nDeseja continuar?\nDigite 1 para SIM ou 2 para NÃO: ");
+                    opcContinua = sc.next().charAt(0);
+
+                    while (opcContinua != '1' && opcContinua != '2') {
+                        System.out.print("Opção inválida. Digite novamente: ");
+                        opcContinua = sc.next().charAt(0);
+                    }
+
+                    if (opcContinua == '1') {
+                        continua = true;
+                    } else {
+                        continua = false;
+                    }
+
+                    break;
+                case '5':
+                    System.out.print("\n=== FIM DO PROGRAMA ===\n");
+                    continua = false;
+                    break;
+            }
+        } while (continua);
+
     }
 }
